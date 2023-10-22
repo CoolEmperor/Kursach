8337804201ae5378d27f1d93cea7ed06487b882c
SuperDima228 <dmitrybugai.live@gmail.com>
2023-10-23 01:16:28 +0300

    Форма Админа Завершена

--- Мастерская/FormAdmin.Designer.cs
+++ Мастерская/FormAdmin.Designer.cs
@@ -46,17 +46,15 @@
             this.buttonEdit = new System.Windows.Forms.Button();
             this.buttonDelete = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.buttonSort = new System.Windows.Forms.Button();
             this.comboBoxSort1 = new System.Windows.Forms.ComboBox();
             this.comboBoxSort2 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.textBoxFindLogin = new System.Windows.Forms.TextBox();
-            this.textBoxFindFam = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFindFam = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.сотрудникBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.мастерскаяDataSet)).BeginInit();
@@ -89,7 +87,7 @@
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1307, 429);
+            this.dataGridView1.Size = new System.Drawing.Size(1307, 460);
             this.dataGridView1.TabIndex = 0;
             // 
             // идСотрудникаDataGridViewTextBoxColumn
@@ -187,7 +185,7 @@
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(4, 25);
+            this.buttonAdd.Location = new System.Drawing.Point(55, 32);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(155, 32);
             this.buttonAdd.TabIndex = 17;
@@ -197,7 +195,7 @@
             // 
             // buttonEdit
             // 
-            this.buttonEdit.Location = new System.Drawing.Point(165, 26);
+            this.buttonEdit.Location = new System.Drawing.Point(216, 33);
             this.buttonEdit.Name = "buttonEdit";
             this.buttonEdit.Size = new System.Drawing.Size(155, 32);
             this.buttonEdit.TabIndex = 18;
@@ -207,9 +205,9 @@
             // 
             // buttonDelete
             // 
-            this.buttonDelete.Location = new System.Drawing.Point(6, 68);
+            this.buttonDelete.Location = new System.Drawing.Point(377, 32);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(316, 32);
+            this.buttonDelete.Size = new System.Drawing.Size(155, 32);
             this.buttonDelete.TabIndex = 19;
             this.buttonDelete.Text = "Удалить";
             this.buttonDelete.UseVisualStyleBackColor = true;
@@ -219,7 +217,6 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.buttonSort);
             this.groupBox1.Controls.Add(this.buttonDelete);
             this.groupBox1.Controls.Add(this.comboBoxSort1);
@@ -228,30 +225,20 @@
             this.groupBox1.Controls.Add(this.buttonAdd);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.textBoxFindLogin);
-            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.textBoxFindFam);
-            this.groupBox1.Location = new System.Drawing.Point(12, 448);
+            this.groupBox1.Location = new System.Drawing.Point(12, 479);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1308, 122);
+            this.groupBox1.Size = new System.Drawing.Size(1308, 87);
             this.groupBox1.TabIndex = 20;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Работа с данными";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(496, 26);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(93, 16);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Живой поиск";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(760, 40);
+            this.label5.Location = new System.Drawing.Point(889, 25);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(41, 16);
             this.label5.TabIndex = 9;
@@ -259,9 +246,9 @@
             // 
             // buttonSort
             // 
-            this.buttonSort.Location = new System.Drawing.Point(998, 44);
+            this.buttonSort.Location = new System.Drawing.Point(1097, 31);
             this.buttonSort.Name = "buttonSort";
-            this.buttonSort.Size = new System.Drawing.Size(218, 33);
+            this.buttonSort.Size = new System.Drawing.Size(155, 33);
             this.buttonSort.TabIndex = 8;
             this.buttonSort.Text = "Сортировать";
             this.buttonSort.UseVisualStyleBackColor = true;
@@ -277,7 +264,7 @@
             "Роль",
             "Телефон",
             "Адрес"});
-            this.comboBoxSort1.Location = new System.Drawing.Point(820, 37);
+            this.comboBoxSort1.Location = new System.Drawing.Point(936, 22);
             this.comboBoxSort1.Name = "comboBoxSort1";
             this.comboBoxSort1.Size = new System.Drawing.Size(155, 24);
             this.comboBoxSort1.TabIndex = 7;
@@ -288,7 +275,7 @@
             this.comboBoxSort2.Items.AddRange(new object[] {
             "Возрастание",
             "Убывание"});
-            this.comboBoxSort2.Location = new System.Drawing.Point(820, 67);
+            this.comboBoxSort2.Location = new System.Drawing.Point(936, 52);
             this.comboBoxSort2.Name = "comboBoxSort2";
             this.comboBoxSort2.Size = new System.Drawing.Size(155, 24);
             this.comboBoxSort2.TabIndex = 6;
@@ -296,54 +283,45 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(760, 70);
+            this.label4.Location = new System.Drawing.Point(898, 55);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(32, 16);
             this.label4.TabIndex = 5;
             this.label4.Text = "Тип";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(783, 18);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(172, 16);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Сортировка по критерию";
-            // 
             // textBoxFindLogin
             // 
-            this.textBoxFindLogin.Location = new System.Drawing.Point(553, 71);
+            this.textBoxFindLogin.Location = new System.Drawing.Point(716, 44);
             this.textBoxFindLogin.Name = "textBoxFindLogin";
             this.textBoxFindLogin.Size = new System.Drawing.Size(160, 22);
             this.textBoxFindLogin.TabIndex = 3;
             this.textBoxFindLogin.TextChanged += new System.EventHandler(this.textBoxFindLogin_TextChanged);
             // 
-            // textBoxFindFam
+            // label1
             // 
-            this.textBoxFindFam.Location = new System.Drawing.Point(378, 71);
-            this.textBoxFindFam.Name = "textBoxFindFam";
-            this.textBoxFindFam.Size = new System.Drawing.Size(160, 22);
-            this.textBoxFindFam.TabIndex = 2;
-            this.textBoxFindFam.TextChanged += new System.EventHandler(this.textBoxFindFam_TextChanged);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(558, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Поиск по Фамилии";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(569, 52);
+            this.label2.Location = new System.Drawing.Point(732, 25);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(116, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Поиск по Логину";
             // 
-            // label1
+            // textBoxFindFam
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(395, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Поиск по Фамилии";
+            this.textBoxFindFam.Location = new System.Drawing.Point(541, 44);
+            this.textBoxFindFam.Name = "textBoxFindFam";
+            this.textBoxFindFam.Size = new System.Drawing.Size(160, 22);
+            this.textBoxFindFam.TabIndex = 2;
+            this.textBoxFindFam.TextChanged += new System.EventHandler(this.textBoxFindFam_TextChanged);
             // 
             // FormAdmin
             // 
@@ -384,9 +362,7 @@
         private System.Windows.Forms.ComboBox comboBoxSort1;
         private System.Windows.Forms.ComboBox comboBoxSort2;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.DataGridViewTextBoxColumn идСотрудникаDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn фамилияDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn имяDataGridViewTextBoxColumn;
